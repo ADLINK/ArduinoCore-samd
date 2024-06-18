972f6df58350554761c70723c83fcf7589595cf2
Joey Haas <joey.haas@adlinktech.com>
2024-06-18 15:11:24 +0800

    Remove Adafruit boards, add I-Pi-OT2IT

--- tools/makeboards.py
+++ tools/makeboards.py
@@ -47,6 +47,16 @@ mcu_dict = {
         'extra_flags': '-D__SAMD51__ -D__FPU_PRESENT -DARM_MATH_CM4 -mfloat-abi=hard -mfpu=fpv4-sp-d16',
         'openocdscript': 'scripts/openocd/daplink_samd51.cfg',
     },
+
+    'SAME54': {
+        'flash_size': 507904,
+        'data_size': 0,
+        'offset': '0x4000',
+        'build_mcu': 'cortex-m4',
+        'f_cpu': '120000000L',
+        'extra_flags': '-D__SAMD51__ -D__FPU_PRESENT -DARM_MATH_CM4 -mfloat-abi=hard -mfpu=fpv4-sp-d16',
+        'openocdscript': 'scripts/openocd/daplink_samd51.cfg',
+    },
 }
 
 
@@ -71,7 +81,7 @@ def build_upload(mcu, name, extra_flags):
     print(f"{name}.upload.tool=bossac18")
     print(f"{name}.upload.protocol=sam-ba")
     
-    if ('SAMD51P20A' in extra_flags) or ('SAMD51J20A' in extra_flags):
+    if ('SAMD51P20A' in extra_flags) or ('SAMD51J20A' in extra_flags) or ('SAME54P20A' in extra_flags) or ('SAME54N20A' in extra_flags):
         flash_size = 1032192
     else:
         flash_size = mcu_dict[mcu]['flash_size']
@@ -113,13 +123,13 @@ def build_build(mcu, name, variant, vendor, product, vid, pid_list, boarddefine,
     print(f"{name}.build.pid={pid_list[0]}")
     print(f"{name}.bootloader.tool=openocd")
     print(f"{name}.bootloader.file={bootloader}")
-    if (mcu == 'SAMD51' or mcu == 'SAME51'):
+    if (mcu == 'SAMD51' or mcu == 'SAME51' or mcu == 'SAME54'):
         print(f'{name}.compiler.arm.cmsis.ldflags="-L{{runtime.tools.CMSIS-5.4.0.path}}/CMSIS/Lib/GCC/" "-L{{build.variant.path}}" -larm_cortexM4lf_math -mfloat-abi=hard -mfpu=fpv4-sp-d16')
     print()
     
 
 def build_menu(mcu, name):
-    if (mcu == 'SAMD51' or mcu == 'SAME51'):
+    if (mcu == 'SAMD51' or mcu == 'SAME51' or mcu == 'SAME54'):
         print("# Menu: Cache")
         print(f"{name}.menu.cache.on=Enabled")
         print(f"{name}.menu.cache.on.build.cache_flags=-DENABLE_CACHE")
@@ -151,7 +161,7 @@ def build_menu(mcu, name):
     print(f"{name}.menu.opt.dragons.build.flags.optimize=-Ofast -funroll-loops")
     print()
     
-    if (mcu == 'SAMD51' or mcu == 'SAME51'):
+    if (mcu == 'SAMD51' or mcu == 'SAME51' or mcu == 'SAME54'):
         print("# Menu: QSPI Speed")
         print(f"{name}.menu.maxqspi.50=50 MHz (standard)")
         print(f"{name}.menu.maxqspi.50.build.flags.maxqspi=-DVARIANT_QSPI_BAUD_DEFAULT=50000000")
@@ -203,212 +213,12 @@ def make_board(mcu, name, variant, vendor, product, vid, pid_list, boarddefine, 
 
 build_global_menu()
 
-# ------------------------------
-# SAM D21 (M0)
-# ------------------------------
-
-# name, variant, vendor, product, vid, pid_list, boarddefine, extra_flags, bootloader
-# try to sort in Alphabetical order
-d21_board_list = [
-    ["adafruit_feather_m0", "feather_m0", "Adafruit", "Feather M0",
-     "0x239A", ["0x800B", "0x000B", "0x0015"],
-     "SAMD_ZERO", "-D__SAMD21G18A__ -DADAFRUIT_FEATHER_M0",
-     "featherM0/bootloader-feather_m0-v2.0.0-adafruit.5.bin"],
-
-    ["adafruit_feather_m0_express", "feather_m0_express", "Adafruit", "Feather M0 Express",
-     "0x239A", ["0x801B", "0x001B"],
-     "SAMD_FEATHER_M0_EXPRESS", "-D__SAMD21G18A__ -DARDUINO_SAMD_FEATHER_M0 -DADAFRUIT_FEATHER_M0_EXPRESS",
-     "featherM0/bootloader-feather_m0-v2.0.0-adafruit.5.bin"],
-
-    ["adafruit_metro_m0", "metro_m0", "Adafruit", "Metro M0 Express",
-     "0x239A", ["0x8013", "0x0013"],
-     "SAMD_ZERO", "-D__SAMD21G18A__ -DADAFRUIT_METRO_M0_EXPRESS",
-     "metroM0/bootloader-metro_m0-v2.0.0-adafruit.5.bin"],
-
-    ["adafruit_circuitplayground_m0", "circuitplay", "Adafruit", "Circuit Playground Express",
-     "0x239A", ["0x8018", "0x0019"],
-     "SAMD_CIRCUITPLAYGROUND_EXPRESS", "-D__SAMD21G18A__ -DCRYSTALLESS -DADAFRUIT_CIRCUITPLAYGROUND_M0",
-     "circuitplayM0/bootloader-circuitplay_m0-v2.0.0-adafruit.5.bin"],
-
-    ["adafruit_gemma_m0", "gemma_m0", "Adafruit", "Gemma M0",
-     "0x239A", ["0x801C", "0x001C"],
-     "GEMMA_M0", "-D__SAMD21E18A__ -DCRYSTALLESS -DADAFRUIT_GEMMA_M0",
-     "gemmaM0/bootloader-gemma_m0-v2.0.0-adafruit.5.bin"],
-
-    ["adafruit_trinket_m0", "trinket_m0", "Adafruit", "Trinket M0",
-     "0x239A", ["0x801E", "0x001E"],
-     "TRINKET_M0", "-D__SAMD21E18A__ -DCRYSTALLESS -DADAFRUIT_TRINKET_M0",
-     "trinketm0/bootloader-trinket_m0-v2.0.0-adafruit.5.bin"],
-
-    ["adafruit_qtpy_m0", "qtpy_m0", "Adafruit", "QT Py M0",
-     "0x239A", ["0x80CB", "0x00CB", "0x00CC"],
-     "QTPY_M0", "-D__SAMD21E18A__ -DCRYSTALLESS -DADAFRUIT_QTPY_M0",
-     "qtpyM0/bootloader-qtpy_m0.bin"],
-
-    ["adafruit_neotrinkey_m0", "neotrinkey_m0", "Adafruit", "NeoPixel Trinkey M0",
-     "0x239A", ["0x80EF", "0x00EF", "0x80F0"],
-     "NEOTRINKEY_M0", "-D__SAMD21E18A__ -DCRYSTALLESS -DADAFRUIT_NEOTRINKEY_M0",
-     "neotrinkey_m0/bootloader-neotrinkey_m0.bin"],
-
-    ["adafruit_rotarytrinkey_m0", "rotarytrinkey_m0", "Adafruit", "Rotary Trinkey M0",
-     "0x239A", ["0x80FB", "0x00FB", "0x80FC"],
-     "ROTARYTRINKEY_M0", "-D__SAMD21E18A__ -DCRYSTALLESS -DADAFRUIT_ROTARYTRINKEY_M0",
-     "rotarytrinkey_m0/bootloader-rotarytrinkey_m0.bin"],
-
-    ["adafruit_neokeytrinkey_m0", "neokeytrinkey_m0", "Adafruit", "NeoKey Trinkey M0",
-     "0x239A", ["0x80FF", "0x00FF", "0x8100"],
-     "NEOKEYTRINKEY_M0", "-D__SAMD21E18A__ -DCRYSTALLESS -DADAFRUIT_NEOKEYTRINKEY_M0",
-     "neokeytrinkey_m0/bootloader-neokeytrinkey_m0.bin"],
-
-    ["adafruit_slidetrinkey_m0", "slidetrinkey_m0", "Adafruit", "Slide Trinkey M0",
-     "0x239A", ["0x8101", "0x0101", "0x8102"],
-     "SLIDETRINKEY_M0", "-D__SAMD21E18A__ -DCRYSTALLESS -DADAFRUIT_SLIDETRINKEY_M0",
-     "slidetrinkey_m0/bootloader-slidetrinkey_m0.bin"],
-
-    ["adafruit_proxlighttrinkey_m0", "proxlighttrinkey_m0", "Adafruit", "ProxLight Trinkey M0",
-     "0x239A", ["0x8103", "0x0103", "0x8104"],
-     "PROXLIGHTTRINKEY_M0", "-D__SAMD21E18A__ -DCRYSTALLESS -DADAFRUIT_PROXLIGHTTRINKEY_M0",
-     "proxlighttrinkey_m0/bootloader-proxlighttrinkey_m0.bin"],
-
-    ["adafruit_sht4xtrinkey_m0", "sht4xtrinkey_m0", "Adafruit", "SHT4x Trinkey M0",
-     "0x239A", ["0x8153", "0x0153", "0x8154"],
-     "SHT4XTRINKEY_M0", "-D__SAMD21E18A__ -DCRYSTALLESS -DADAFRUIT_SHT4XTRINKEY_M0",
-     "sht4xtrinkey_m0/bootloader-sht4xtrinkey_m0.bin"],
-
-    ["adafruit_pixeltrinkey_m0", "pixeltrinkey_m0", "Adafruit", "Pixel Trinkey M0",
-     "0x239A", ["0x8155", "0x0155", "0x8156"],
-     "PIXELTRINKEY_M0", "-D__SAMD21E18A__ -DCRYSTALLESS -DADAFRUIT_PIXELTRINKEY_M0",
-     "pixeltrinkey_m0/bootloader-pixeltrinkey_m0.bin"],
-
-    ["adafruit_TRRStrinkey_m0", "TRRStrinkey_m0", "Adafruit", "TRRS Trinkey M0",
-     "0x239A", ["0x8157", "0x0157", "0x8158"],
-     "TRRSTRINKEY_M0", "-D__SAMD21E18A__ -DCRYSTALLESS -DADAFRUIT_TRRSTRINKEY_M0",
-     "TRRStrinkey_m0/bootloader-TRRStrinkey_m0.bin"],
-
-    ["adafruit_thumbsticktrinkey_m0", "thumbsticktrinkey_m0", "Adafruit", "Thumbstick Trinkey M0",
-     "0x239A", ["0x8159", "0x0159", "0x8160"],
-     "THUMBSTICKTRINKEY_M0", "-D__SAMD21E18A__ -DCRYSTALLESS -DADAFRUIT_THUMBSTICKTRINKEY_M0",
-     "thumbsticktrinkey_m0/bootloader-thumbsticktrinkey_m0.bin"],
-
-    ["adafruit_itsybitsy_m0", "itsybitsy_m0", "Adafruit", "ItsyBitsy M0 Express",
-     "0x239A", ["0x800F", "0x000F", "0x8012"],
-     "ITSYBITSY_M0", "-D__SAMD21G18A__ -DCRYSTALLESS -DADAFRUIT_ITSYBITSY_M0",
-     "itsybitsyM0/bootloader-itsybitsy_m0-v2.0.0-adafruit.5.bin"],
-
-    ["adafruit_pirkey", "pirkey", "Adafruit", "pIRKey",
-     "0x239A", ["0x8027", "0x0027", "0x8028"],
-     "PIRKEY", "-D__SAMD21E18A__ -DCRYSTALLESS -DADAFRUIT_PIRKEY",
-     "pirkey/bootloader-pirkey-v2.0.0-adafruit.5.bin"],
-
-    ["adafruit_hallowing", "hallowing_m0_express", "Adafruit", "Hallowing M0",
-     "0x239A", ["0xDEAD", "0xD1ED", "0xB000"],
-     "SAMD_HALLOWING", "-D__SAMD21G18A__ -DCRYSTALLESS -DARDUINO_SAMD_HALLOWING_M0 -DADAFRUIT_HALLOWING",
-     "hallowingM0/bootloader-hallowing_m0-v2.0.0-adafruit.0-21-g887cc30.bin"],
-
-    ["adafruit_crickit_m0", "crickit_m0", "Adafruit", "Crickit M0",
-     "0x239A", ["0x802D", "0x002D", "0x802D"],
-     "CRICKIT_M0", "-D__SAMD21G18A__ -DCRYSTALLESS -DADAFRUIT_CRICKIT_M0",
-     "crickit/samd21_sam_ba.bin"],
-
-    ["adafruit_blm_badge", "blm_badge", "Adafruit", "BLM Badge",
-     "0x239A", ["0x80BF", "0x00BF", "0x80C0"],
-     "BLM_BADGE_M0", "-D__SAMD21E18A__ -DCRYSTALLESS -DADAFRUIT_BLM_BADGE",
-     "blmbadge/bootloader-blm_badge.bin"],
-]
-
-for b in d21_board_list:
-    make_board("SAMD21", b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7], b[8])
-
-
-# ----------------------------
-# SAM D51 and E51 (M4)
-# ----------------------------
-
-d51_board_list = [
-    ["adafruit_metro_m4", "metro_m4", "Adafruit", "Metro M4",
-     "0x239A", ["0x8020", "0x0020", "0x8021", "0x0021"],
-     "METRO_M4", "-D__SAMD51J19A__ -DADAFRUIT_METRO_M4_EXPRESS",
-     "metroM4/bootloader-metro_m4-v2.0.0-adafruit.5.bin"],
-
-    ["adafruit_grandcentral_m4", "grand_central_m4", "Adafruit", "Grand Central M4",
-     "0x239A", ["0x8031", "0x0031", "0x0032"],
-     "GRAND_CENTRAL_M4", "-D__SAMD51P20A__ -DADAFRUIT_GRAND_CENTRAL_M4",
-     "grand_central_m4/bootloader-grandcentral_m4.bin"],
-
-    ["adafruit_itsybitsy_m4", "itsybitsy_m4", "Adafruit", "ItsyBitsy M4",
-     "0x239A", ["0x802B", "0x002B"],
-     "ITSYBITSY_M4", "-D__SAMD51G19A__ -DCRYSTALLESS -DADAFRUIT_ITSYBITSY_M4_EXPRESS",
-     "itsybitsyM4/bootloader-itsybitsy_m4-v2.0.0-adafruit.5.bin"],
-
-    ["adafruit_feather_m4", "feather_m4", "Adafruit", "Feather M4 Express",
-     "0x239A", ["0x8022", "0x0022", "0x8026"],
-     "FEATHER_M4", "-D__SAMD51J19A__ -DADAFRUIT_FEATHER_M4_EXPRESS",
-     "featherM4/bootloader-feather_m4-v2.0.0-adafruit.5.bin"],
-
-    ["adafruit_feather_m4_can", "feather_m4_can", "Adafruit", "Feather M4 CAN",
-     "0x239A", ["0x80CD", "0x00CD"],
-     "FEATHER_M4_CAN", "-D__SAME51J19A__ -DADAFRUIT_FEATHER_M4_EXPRESS -DADAFRUIT_FEATHER_M4_CAN",
-     "featherM4/bootloader-feather_m4_express-v2.0.0-adafruit.5.bin"],
-
-    # ["adafruit_feather_m4_log", "feather_m4_log", "Adafruit", "Feather M4 Adalogger",
-    #  "0x239A", ["0x8115", "0x0115", "0x8116"],
-    #  "FEATHER_M4_ADALOGGER", "-D__SAME51J19A__ -DADAFRUIT_FEATHER_M4_ADALOGGER",
-    #  "feather_m4_log/bootloader-feather_m4_log-v2.0.0-adafruit.5.bin"],
-
-    ["adafruit_trellis_m4", "trellis_m4",
-     "Adafruit", "Trellis M4", "0x239A", ["0x802F", "0x002F", "0x0030"],
-     "TRELLIS_M4", "-D__SAMD51G19A__ -DCRYSTALLESS -DADAFRUIT_TRELLIS_M4_EXPRESS",
-     "trellisM4/bootloader-trellis_m4-v2.0.0-adafruit.5.bin"],
-
-    ["adafruit_pyportal_m4", "pyportal_m4", "Adafruit", "PyPortal M4",
-     "0x239A", ["0x8035", "0x0035", "0x8036"],
-     "PYPORTAL_M4", "-D__SAMD51J20A__ -DCRYSTALLESS -DADAFRUIT_PYPORTAL",
-     "metroM4/bootloader-metro_m4-v2.0.0-adafruit.5.bin"],
-
-    ["adafruit_pyportal_m4_titano", "pyportal_m4_titano", "Adafruit", "PyPortal M4 Titano",
-     "0x239A", ["0x8053", "0x8053"],
-     "PYPORTAL_M4_TITANO", "-D__SAMD51J20A__ -DCRYSTALLESS -DADAFRUIT_PYPORTAL_M4_TITANO",
-     "metroM4/bootloader-metro_m4-v2.0.0-adafruit.5.bin"],
-
-    ["adafruit_pybadge_m4", "pybadge_m4", "Adafruit", "pyBadge M4 Express",
-     "0x239A", ["0x8033", "0x0033", "0x8034", "0x0034"],
-     "PYBADGE_M4", "-D__SAMD51J19A__ -DCRYSTALLESS -DADAFRUIT_PYBADGE_M4_EXPRESS",
-     "featherM4/bootloader-feather_m4-v2.0.0-adafruit.5.bin"],
-
-    ["adafruit_metro_m4_airliftlite", "metro_m4_airlift", "Adafruit", "Metro M4 AirLift Lite",
-     "0x239A", ["0x8037", "0x0037"],
-     "METRO_M4_AIRLIFT_LITE", "-D__SAMD51J19A__ -DADAFRUIT_METRO_M4_AIRLIFT_LITE",
-     "metroM4/bootloader-metro_m4-v2.0.0-adafruit.5.bin"],
-
-    ["adafruit_pygamer_m4", "pygamer_m4", "Adafruit", "PyGamer M4 Express",
-     "0x239A", ["0x803D", "0x003D", "0x803E"],
-     "PYGAMER_M4", "-D__SAMD51J19A__ -DCRYSTALLESS -DADAFRUIT_PYGAMER_M4_EXPRESS",
-     "featherM4/bootloader-feather_m4-v2.0.0-adafruit.5.bin"],
-
-    ["adafruit_pybadge_airlift_m4", "pybadge_airlift_m4", "Adafruit", "pyBadge AirLift M4",
-     "0x239A", ["0x8043", "0x0043", "0x8044"],
-     "PYBADGE_AIRLIFT_M4", "-D__SAMD51J20A__ -DCRYSTALLESS -DADAFRUIT_PYBADGE_AIRLIFT_M4",
-     "featherM4/bootloader-feather_m4-v2.0.0-adafruit.5.bin"],
-
-    ["adafruit_monster_m4sk", "monster_m4sk", "Adafruit", "MONSTER M4SK",
-     "0x239A", ["0x8047", "0x0047", "0x8048"],
-     "MONSTER_M4SK", "-D__SAMD51G19A__ -DCRYSTALLESS -DADAFRUIT_MONSTER_M4SK_EXPRESS",
-     "featherM4/bootloader-feather_m4-v2.0.0-adafruit.5.bin"],
-
-    ["adafruit_hallowing_m4", "hallowing_m4", "Adafruit", "Hallowing M4",
-     "0x239A", ["0x8049", "0x0049", "0x804A"],
-     "HALLOWING_M4", "-D__SAMD51J19A__ -DCRYSTALLESS -DADAFRUIT_HALLOWING_M4_EXPRESS",
-     "featherM4/bootloader-feather_m4-v2.0.0-adafruit.5.bin"],
-
-    ["adafruit_matrixportal_m4", "matrixportal_m4", "Adafruit", "Matrix Portal M4",
-     "0x239A", ["0x80C9", "0x00C9", "0x80CA"],
-     "MATRIXPORTAL_M4", "-D__SAMD51J19A__ -DCRYSTALLESS  -DADAFRUIT_MATRIXPORTAL_M4_EXPRESS",
-     "matrixportalM4/bootloader-matrixportal_m4.bin"],
-]
-
-for b in d51_board_list:
-    # M4 CAN is the only SAME51
-    if b[0] == "adafruit_feather_m4_can":
-        make_board("SAME51", b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7], b[8])
-    else:
-        make_board("SAMD51", b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7], b[8])
+make_board("SAME54", 
+           "adlink_ipi_ot2it", 
+           "ipi_ot2it", 
+           "Adlink", 
+           "I-Pi-OT2IT", 
+           "0x0B63", 
+           ["0xD101", "0xD102", "0xD103"], 
+           "IPI_OT2IT", "-D__SAME54P20A__ -DADLINK_IPI_OT2IT", 
+           "ipi_ot2it/bootloader-ipi-ot2it.bin");
